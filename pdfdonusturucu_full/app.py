2bfc23526e6fc3ac3882752f565dad7d22996f6b
yagmurarp <yagmurarpaci5@gmail.com>
2025-08-10 23:17:30 +0300

    Update app.py

--- pdfdonusturucu_full/app.py
+++ pdfdonusturucu_full/app.py
@@ -43,5 +43,8 @@ def convert():
         try: os.remove(src)
         except Exception: pass
 
-if __name__=='__main__':
-    port=int(os.environ.get('PORT',8080)); app.run(host='0.0.0.0',port=port)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
